4cc57b589e9d02be112682441df9a4dff7e0a72a
momoladebrouill <momoladebrouill@gmail.com>
2021-08-01 17:43:55 +0200

    hihi une version sympa
    on est bientôt fini

--- catchgame.py
+++ catchgame.py
@@ -19,34 +19,32 @@ class Pos:
         self.x=x
         self.y=y
 class Carre:
-    def __init__(self,x=WIND/2,y=WIND/2,cote=100):
+    def __init__(self,x=WIND/2,y=WIND/2,cote=200):
         self.x=x
         self.y=y
-        self.cote=cote
+        self.diag=cote
         self.angle=0
-        self.nbs=4
+        self.nbs=level
     def draw(self):
-        
-        points=[]
-        for j in range(1,self.nbs+1):
-            i=j*math.tau/self.nbs+math.pi/2
-            points.append((self.x+math.cos(self.angle+i)*self.cote,
-                           self.y+math.sin(self.angle+i)*self.cote))
-        #pg.draw.polygon(f,(255,255,255),points)
-        for i in range(len(points)-1):
-            pg.draw.line(f,hsv((i+1)/(len(points)-1),1,255),points[i+1],points[i])
-        pg.draw.line(f,hsv(1,1,255),points[0],points[-1])
+        for cot in range(level):
+            pg.draw.arc(f,
+                        hsv_to_rgb(cot/level,1,255),
+                        pg.Rect(self.x-self.diag/2,self.y-self.diag/2,self.diag,self.diag),
+                        math.tau*cot/level+self.angle,
+                        math.tau*(cot+1)/level+self.angle,
+                        int(self.diag/4))
 
 class Missile:
     def __init__(self):
         self.attack=random.random()*math.tau
-        self.x=math.cos(self.attack)*WIND
-        self.y=math.sin(self.attack)*WIND
+        self.x,self.y=(-math.cos(self.attack)+1)*WIND/2,(1-math.sin(self.attack))*WIND/2
+        
         self.coul=hsv(int(random.random()*level)/level,1,255)
-        self.dir=(self.x/self.x*-1,self.y/self.y*-1)
+        self.dir=(math.cos(self.attack),math.sin(self.attack))
         self.angle=0
     def draw(self):
         points=[]
+        self.coul=self.test_good()
         self.angle+=0.1
         self.angle=self.angle%math.tau
         for j in range(1,4):
@@ -56,7 +54,11 @@ class Missile:
         pg.draw.polygon(f,self.coul,points)
         self.x+=self.dir[0]
         self.y+=self.dir[1]
-        
+    def test_good(self):
+        self.val=math.atan2((WIND/2-self.y),(self.x-WIND/2))-forme.angle
+        if self.val<0:self.val+=math.tau
+        self.val=self.val%math.tau
+        return hsv((int(self.val/math.tau*level))/level,1,255)
 b = 1
 forme=Carre()
 mouv=10
@@ -67,26 +69,23 @@ try:
         # Actualiser:
         pg.display.flip()
         # Appliquer les images de fond sur la fenetre
-        s = pg.Surface((WIND, WIND))  
-        s.set_alpha(360)
-        s.fill((0, 0, 0))
-        f.blit(s, (0, 0))
+        f.fill(0)
+        
         forme.draw()
         si = pg.key.get_pressed()  # SI la touche est appuyée
-        if si[pg.K_z]:forme.y-=mouv
-        if si[pg.K_q]:forme.x-=mouv
-        if si[pg.K_s]:forme.y+=mouv
-        if si[pg.K_d]:forme.x+=mouv
-        if si[pg.K_RIGHT]:forme.angle+=math.pi/64
-        if si[pg.K_LEFT]:forme.angle-=math.pi/64
+       
+        if si[pg.K_RIGHT]:forme.angle-=math.pi/64
+        if si[pg.K_LEFT]:forme.angle+=math.pi/64
         if si[pg.K_a]:wow.append(Missile())
         next_wow=[]
         for i in wow:
             i.draw()
-            if dist(i,Pos(WIND/2,WIND/2))>forme.cote:
+            if 0<i.x<WIND and 0<i.y<WIND and dist(i,forme)>forme.diag/2:
                 next_wow.append(i)
-            else:
-                ouverture=math.tau/level
+            elif dist(i,forme)<forme.diag/2:
+                i.__init__()
+                next_wow.append(i)
+            
         wow=next_wow[:]
         if not b%100:
             wow.append(Missile())
@@ -97,9 +96,12 @@ try:
             elif event.type == pg.KEYUP:
                 key=event.dict['key']
                 if key==pg.K_UP:
-                    forme.nbs+=1
+                    
+                    level+=1
                 if key==pg.K_DOWN:
-                    forme.nbs-=1
+                  
+                    level-=1
+                    level=abs(level-1)+1
                 if key==pg.K_ESCAPE:
                     forme.angle=0
             elif event.type == pg.MOUSEBUTTONUP:
@@ -108,9 +110,9 @@ try:
                 if event.button==3: #click droit"""
                     forme.nbs-=1
                 if event.button==4: #vers le haut
-                    forme.cote+=10
+                    forme.diag+=10
                 elif event.button==5: #vers le bas
-                    forme.cote-=10
+                    forme.diag-=10
 
 
 
