4aa3cbcddb6e499c26af92548011b0715c7b43e3
momoladebrouill <momoladebrouill@gmail.com>
2021-08-01 18:52:07 +0200

    Améliorations poggers

--- catchgame.py
+++ catchgame.py
@@ -11,7 +11,7 @@ pg.init()
 f = pg.display.set_mode(size=(WIND, WIND))
 pg.display.set_caption("")
 fpsClock = pg.time.Clock()
-font=pg.font.SysFont('consolas',52)
+font=pg.font.SysFont('Impact',52)
 level=3
 dist=lambda a,b:math.sqrt((a.x-b.x)**2+(a.y-b.y)**2)
 class Pos:
@@ -28,23 +28,28 @@ class Carre:
     def draw(self):
         for cot in range(level):
             pg.draw.arc(f,
-                        hsv_to_rgb(cot/level,1,255),
+                        hsv_to_rgb(cot/level,0.5,100),
                         pg.Rect(self.x-self.diag/2,self.y-self.diag/2,self.diag,self.diag),
                         math.tau*cot/level+self.angle,
                         math.tau*(cot+1)/level+self.angle,
-                        int(self.diag/4))
+                        int(self.diag/8))
+            pg.draw.arc(f,
+                        hsv_to_rgb(cot/level,0.5,255),
+                        pg.Rect(self.x-self.diag/2,self.y-self.diag/2,self.diag,self.diag),
+                        math.tau*cot/level+self.angle,
+                        math.tau*(cot+1)/level+self.angle,
+                        int(self.diag/16))
 
 class Missile:
     def __init__(self):
         self.attack=random.random()*math.tau
         self.x,self.y=(-math.cos(self.attack)+1)*WIND/2,(1-math.sin(self.attack))*WIND/2
-        
-        self.coul=hsv(int(random.random()*level)/level,1,255)
+        self.teinte=int(random.random()*level)/level
+        self.coul=hsv(self.teinte,0.5,255)
         self.dir=(math.cos(self.attack),math.sin(self.attack))
         self.angle=0
     def draw(self):
         points=[]
-        self.coul=self.test_good()
         self.angle+=0.1
         self.angle=self.angle%math.tau
         for j in range(1,4):
@@ -52,20 +57,28 @@ class Missile:
             points.append((self.x+math.cos(self.angle+i)*10,
                            self.y+math.sin(self.angle+i)*10))
         pg.draw.polygon(f,self.coul,points)
+    def mouve(self):
         self.x+=self.dir[0]
         self.y+=self.dir[1]
     def test_good(self):
         self.val=math.atan2((WIND/2-self.y),(self.x-WIND/2))-forme.angle
         if self.val<0:self.val+=math.tau
         self.val=self.val%math.tau
-        return hsv((int(self.val/math.tau*level))/level,1,255)
+        return int(self.val/math.tau*level)/level==self.teinte
 b = 1
 forme=Carre()
 mouv=10
 wow=[]
+score=0
+IsPause=False
+dificulty=3
 try:
+    pg.mixer.music.load("chill.wav")
+    pg.mixer.music.set_volume(1)
+    pg.mixer.music.play(-1) 
     while b:
-        b+=1
+        if not IsPause:
+            b+=1
         # Actualiser:
         pg.display.flip()
         # Appliquer les images de fond sur la fenetre
@@ -73,18 +86,22 @@ try:
         
         forme.draw()
         si = pg.key.get_pressed()  # SI la touche est appuyée
-       
-        if si[pg.K_RIGHT]:forme.angle-=math.pi/64
-        if si[pg.K_LEFT]:forme.angle+=math.pi/64
+        if not IsPause:
+            if si[pg.K_RIGHT]:forme.angle-=math.pi/64
+            if si[pg.K_LEFT]:forme.angle+=math.pi/64
         if si[pg.K_a]:wow.append(Missile())
         next_wow=[]
         for i in wow:
+            if IsPause==False:i.mouve()
             i.draw()
             if 0<i.x<WIND and 0<i.y<WIND and dist(i,forme)>forme.diag/2:
                 next_wow.append(i)
             elif dist(i,forme)<forme.diag/2:
-                i.__init__()
-                next_wow.append(i)
+                pg.mixer.Sound("switch.mp3").play()
+                if i.test_good():
+                    score+=1
+                else:
+                    score-=1
             
         wow=next_wow[:]
         if not b%100:
@@ -95,13 +112,8 @@ try:
                 print(" Fin du jeu  babe")
             elif event.type == pg.KEYUP:
                 key=event.dict['key']
-                if key==pg.K_UP:
-                    
-                    level+=1
-                if key==pg.K_DOWN:
-                  
-                    level-=1
-                    level=abs(level-1)+1
+                if event.key==pg.K_SPACE:
+                    IsPause= not IsPause
                 if key==pg.K_ESCAPE:
                     forme.angle=0
             elif event.type == pg.MOUSEBUTTONUP:
@@ -113,7 +125,16 @@ try:
                     forme.diag+=10
                 elif event.button==5: #vers le bas
                     forme.diag-=10
-
+        if int(score/10)+3!=level:
+            level=int(score/10)+3
+            wow=[]
+            pg.mixer.Sound("strike.mp3").play()
+      
+        s_core=font.render(str(score),1,(255,255,255))
+        s_core_rect=s_core.get_rect()
+        f.blit(s_core,(WIND/2-s_core_rect.width/2,WIND/2-s_core_rect.height/2))
+        if IsPause:
+            f.blit(font.render("Pause",1,(255,255,255)),(0,0))
 
 
         fpsClock.tick(FPS)
