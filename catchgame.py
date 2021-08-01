b67eaada1ae93929d8060a4791d9e6a14bef8a24
momoladebrouill <momoladebrouill@gmail.com>
2021-08-01 19:46:27 +0200

    Update catchgame.py

--- catchgame.py
+++ catchgame.py
@@ -8,10 +8,11 @@ hsv=hsv_to_rgb
 FPS = 60  # les fps tabernak
 WIND = 750 # dimentions de la fentere
 pg.init()
-f = pg.display.set_mode(size=(WIND, WIND))
-pg.display.set_caption("")
+f = pg.display.set_mode((WIND, WIND))
+pg.display.set_caption("THE TRIGONOMETRISATOR")
 fpsClock = pg.time.Clock()
 font=pg.font.SysFont('Impact',52)
+sub_font=pg.font.SysFont('Impact',26)
 level=3
 dist=lambda a,b:math.sqrt((a.x-b.x)**2+(a.y-b.y)**2)
 class Pos:
@@ -27,18 +28,15 @@ class Carre:
         self.nbs=level
     def draw(self):
         for cot in range(level):
-            pg.draw.arc(f,
-                        hsv_to_rgb(cot/level,0.5,100),
-                        pg.Rect(self.x-self.diag/2,self.y-self.diag/2,self.diag,self.diag),
-                        math.tau*cot/level+self.angle,
-                        math.tau*(cot+1)/level+self.angle,
-                        int(self.diag/8))
-            pg.draw.arc(f,
-                        hsv_to_rgb(cot/level,0.5,255),
-                        pg.Rect(self.x-self.diag/2,self.y-self.diag/2,self.diag,self.diag),
-                        math.tau*cot/level+self.angle,
-                        math.tau*(cot+1)/level+self.angle,
-                        int(self.diag/16))
+            nb=2
+            for i in range(nb):
+                pg.draw.arc(f,
+                            hsv_to_rgb(cot/level,0.5,(i+1)/nb*255),
+                            pg.Rect(self.x-self.diag/2,self.y-self.diag/2,self.diag,self.diag),
+                            math.tau*cot/level+self.angle,
+                            math.tau*(cot+1)/level+self.angle,
+                            int(self.diag/2**(3+i)))
+            
 
 class Missile:
     def __init__(self):
@@ -52,8 +50,9 @@ class Missile:
         points=[]
         self.angle+=0.1
         self.angle=self.angle%math.tau
-        for j in range(1,4):
-            i=j*math.tau/3+math.pi/2
+        
+        for j in range(1,level+1):
+            i=j*math.tau/level+math.pi/2
             points.append((self.x+math.cos(self.angle+i)*10,
                            self.y+math.sin(self.angle+i)*10))
         pg.draw.polygon(f,self.coul,points)
@@ -75,7 +74,55 @@ dificulty=3
 try:
     pg.mixer.music.load("chill.wav")
     pg.mixer.music.set_volume(1)
-    pg.mixer.music.play(-1) 
+    pg.mixer.music.play(-1)
+    forme.y=200
+    forme.draw()
+
+    title=font.render('The Trigonometrisator',1,(100,100,100))
+    titlerect=title.get_rect()
+    f.blit(title,(WIND/2-titlerect.width/2+5,WIND/2-titlerect.height/2+5))
+    
+    title=font.render('The Trigonometrisator',1,(255,255,255))
+    f.blit(title,(WIND/2-titlerect.width/2,WIND/2-titlerect.height/2))
+    decal=WIND/2+titlerect.height/2
+    
+    arrow=sub_font.render('Flèches de gauche et de droite pour tourner le disque',1,(200,200,200))
+    arrowrect=arrow.get_rect()
+    f.blit(arrow,(WIND/2-arrowrect.width/2,decal))
+    decal+=arrowrect.height
+
+    arrow=sub_font.render('Presser A pour faire le gue-din',1,(200,200,200))
+    arrowrect=arrow.get_rect()
+    f.blit(arrow,(WIND/2-arrowrect.width/2,decal))
+    decal+=arrowrect.height
+    
+    space=sub_font.render('Barre espace pour une pause ou lancer le JEUVIDEOGAMING',1,(200,200,200))
+    f.blit(space,(WIND/2-space.get_rect().width/2,decal))
+    
+    pg.display.flip()
+    affecter=0
+    while b:
+        pg.display.flip()
+        for event in pg.event.get():
+            if event.type==pg.QUIT:
+                pg.quit()
+                raise IndexError
+            elif event.type==pg.KEYDOWN:
+                if event.key==pg.K_SPACE:
+                    b=0
+                elif event.key==pg.K_RIGHT:
+                    affecter=math.pi/64
+                elif event.key==pg.K_LEFT:
+                    affecter=-math.pi/64
+                else:
+                    affecter=0
+            elif event.type==pg.KEYUP:
+                affecter=0
+        if affecter:
+            forme.angle+=affecter/10
+            forme.draw()
+    b=1
+    forme.y=WIND/2
     while b:
         if not IsPause:
             b+=1
@@ -110,7 +157,7 @@ try:
             if event.type == pg.QUIT:
                 b = False
                 print(" Fin du jeu  babe")
-            elif event.type == pg.KEYUP:
+            elif event.type == pg.KEYDOWN:
                 key=event.dict['key']
                 if event.key==pg.K_SPACE:
                     IsPause= not IsPause
@@ -132,9 +179,12 @@ try:
       
         s_core=font.render(str(score),1,(255,255,255))
         s_core_rect=s_core.get_rect()
+        f.blit(font.render(str(score),1,(100,100,100)),(WIND/2-s_core_rect.width/2+5,WIND/2-s_core_rect.height/2+5))
         f.blit(s_core,(WIND/2-s_core_rect.width/2,WIND/2-s_core_rect.height/2))
+        
         if IsPause:
-            f.blit(font.render("Pause",1,(255,255,255)),(0,0))
+            f.blit(font.render("Pause-hin",1,(255,255,255)),(0,0))
+            f.blit(sub_font.render("Allez hop E prépausal dans ta geule",1,(100,100,100)),(0,50))
 
 
         fpsClock.tick(FPS)
